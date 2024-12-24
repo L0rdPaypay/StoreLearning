b2dca06f7b967cb4793aa85b7ada4bfa203ac1cb
L0rdPaypay <aboba@ro.ru>
2024-12-24 20:21:01 +0300

    boghe

---
+++ infrastructure3/Store.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Aboba-San"),
+            new Book(2, "Naruto"),
+            new Book(3, "Konoha"),
+        };
+
+        Book[] IBookRepository.GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
