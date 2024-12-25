741a8cbd069712d0f076d78df5fcd1b0daec2d84
L0rdPaypay <aboba@ro.ru>
2024-12-25 17:48:44 +0300

    unitTests for IsIsbn

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -8,9 +8,8 @@ namespace Store
 {
     public interface IBookRepository
     {
-        Book[] GetAllByTitle(string titlePart)
-        {
-            throw new NotImplementedException();
-        }
+        Book[] GetAllByIsbn(string isbn);
+
+        Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
     }
 }
--- infrastructure3/Store.Memory/BookRepository.cs
+++ infrastructure3/Store.Memory/BookRepository.cs
@@ -7,12 +7,17 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Aboba-San"),
-            new Book(2, "Naruto"),
-            new Book(3, "Konoha"),
+            new Book(1, "ISBN 11111-11111", "Ya. Napisal", "Aboba-San"),
+            new Book(2, "ISBN 22222-22222", "Kto-to tam napisal", "Naruto"),
+            new Book(3, "ISBN 33333-33333", "eto voobshe derevnya", "Konoha"),
         };
 
-        Book[] IBookRepository.GetAllByTitle(string titlePart)
+        public Book[] GetAllByIsbn(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        Book[] IBookRepository.GetAllByTitleOrAuthor(string titlePart)
         {
             return books.Where(book => book.Title.Contains(titlePart))
                         .ToArray();
