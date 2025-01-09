8191b11482c7f8bcd619fc3cad7c66cc5c61f392
L0rdPaypay <aboba@ro.ru>
2025-01-09 18:40:35 +0300

    search by id. full book description by ref in html

--- infrastructure3/Store.Memory/BookRepository.cs
+++ infrastructure3/Store.Memory/BookRepository.cs
@@ -7,15 +7,20 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 11111-11111", "Ya. Napisal", "Aboba-San"),
-            new Book(2, "ISBN 22222-22222", "Kto-to tam napisal", "Naruto"),
-            new Book(3, "ISBN 33333-33333", "eto voobshe derevnya", "Konoha"),
+            new Book(1, "ISBN 11111-11111", "Ya. Napisal", "Aboba-San", "OOOchen` interesnay kniga", 7.19m),
+            new Book(2, "ISBN 22222-22222", "Kto-to tam napisal", "Naruto", "Ya Naruto Yzumaki, ya stany velikim Hokage", 12.45m),
+            new Book(3, "ISBN 33333-33333", "eto voobshe derevnya", "Konoha", "Listik delaet` pym pym. Texnika 1000 letney boli", 69.99m),
         };
 
         public Book[] GetAllByIsbn(string isbn)
         {
             return books.Where(book =>  book.Isbn == isbn)
                         .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
 
         Book[] IBookRepository.GetAllByTitleOrAuthor(string query)
